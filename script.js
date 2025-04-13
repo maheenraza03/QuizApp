14c163613ad8e973e6d46b70355557bf1a9f9599
Maheen Raza <124949405+maheenraza03@users.noreply.github.com>
2025-04-13 18:58:28 +0000

    working on js

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-function startGame() {
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
+const shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    
 }
 
 function selectAnswer() {
     
 }
+
+const questions = [
+    {
+        question: 'What is 2+2?',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+    }
+]
