91e4b4a2f9e71444ffa4068d381a6371061d6ca0
Maheen Raza <124949405+maheenraza03@users.noreply.github.com>
2025-04-20 00:50:14 +0000

    finishing up showQuestion

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons') 
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -21,10 +21,20 @@ function setNextQuestion() {
 }
 
 function showQuestion(question) {
-
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
     
 }
 
